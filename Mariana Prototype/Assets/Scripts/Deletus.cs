446b600ca31ba2f312e2b4b73d4bad0f47edc575
RatebLeahcim <micaitylstudio@gmail.com>
2023-09-13 09:53:09 -0400

    Added Interaction for Story elements.

--- Mariana Prototype/Assets/Scripts/Deletus.cs
+++ Mariana Prototype/Assets/Scripts/Deletus.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Deletus : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject object1;
+    [SerializeField]
+    private GameObject object2;
+    
     private void OnMouseDown()
     {
         // Destroy the block when it is clicked
         Destroy(gameObject);
+        if (object2 == null)
+        {
+            object1.SetActive(true);
+        }
     }
 }
